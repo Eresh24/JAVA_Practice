1ea9c7e7c75cf5aa392489302269844245a13612
Iresh Maharjan <iresh.maharjan24@gmail.com>
2024-10-27 21:21:31 +1100

    Classes, Objects and Methods in Java

---
+++ src/Animal.java
@@ -0,0 +1,21 @@
+public class Animal {
+     public static void main (String[] args){
+         //Animal is
+        Animal buzo = new Animal();
+            buzo.eat();
+            buzo.run();
+     }
+     public void eat(){
+         System.out.println("i am eating");
+     }
+     public void run(){
+         System.out.println("seema was running");
+     }
+}
+//Access modifier: default,public,private,protected
+//creating class
+//Return type: void
+/*
+* Object created buzo
+* calling the method: buzo.eat()
+* */
